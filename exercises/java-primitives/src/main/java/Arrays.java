97fbfae5dee1d1c8b48b886edef4a7206b908d96
Tolulope Olobayo <tolobayo1@gmail.com>
2024-07-25 21:42:05 -0400

    complete primitives exercises

---
+++ exercises/java-primitives/src/main/java/Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] integers = {1,2,3,4,5,6,7,8};
+        float[] floats = new float[12];
+        double[] doubles = new double[5];
+        boolean[] booleans = new boolean[6];
+
+        Object[][] maxtrix = {};
+
+        System.out.println(integers[4]);
+
+    }
+}
