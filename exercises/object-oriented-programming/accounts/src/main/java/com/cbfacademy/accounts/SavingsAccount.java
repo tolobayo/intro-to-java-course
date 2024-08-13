88bb3cd5699d1525e7ce9b28c84da31d6413750f
tolobayo <tolobayo1@gmail.com>
2024-08-12 20:00:18 -0400

    work on oop exercises

--- exercises/object-oriented-programming/accounts/src/main/java/com/cbfacademy/accounts/SavingsAccount.java
+++ exercises/object-oriented-programming/accounts/src/main/java/com/cbfacademy/accounts/SavingsAccount.java
@@ -1,8 +1,22 @@
 package com.cbfacademy.accounts;
 
 public class SavingsAccount extends Account {
+    public double interestRate;
+
     public SavingsAccount(int accountNumber, double balance, double interestRate) {
         super(accountNumber, balance);
+        this.interestRate = interestRate;
     }
-    
+
+    public void applyInterest(double rate) {
+        interestRate = rate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate() {
+        
+    }
 }
